51a1db595def68ac91ca06f2ae3ad1700a0c0821
Álvaro Zájara <alvaro.zajara@eviden.com>
2024-11-24 16:45:50 +0100

    Adding logic to App to make Todo functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { Todo } from "./Components/Todo/Todo";
 
 function App() {
-  return (
-    <>
-      <h1>Hello world!</h1>
-      <Todo />
-    </>
-  );
+  const [todo, setTodo] = useState({
+    id: 1,
+    title: "This is a todo",
+    plannedStart: new Date(),
+    plannedEnd: new Date(),
+    isCompleted: false,
+  });
+
+  const handleCompleted = (id: number): void => {
+    console.log("finding id...", id);
+    setTodo((prevState) => ({
+      ...prevState,
+      isCompleted: !prevState.isCompleted,
+    }));
+  };
+
+  return <Todo toggleCompleted={handleCompleted} todoDetails={todo} />;
 }
 
 export default App;
