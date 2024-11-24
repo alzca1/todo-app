c86a46c9072ae1f445925c70164d178cf89c54ff
Álvaro Zájara <alvaro.zajara@eviden.com>
2024-11-24 16:45:28 +0100

    adding basic logic to Todo in order to test it

--- src/Components/Todo/Todo.tsx
+++ src/Components/Todo/Todo.tsx
@@ -1,10 +1,39 @@
+import { Checkbox, FormControlLabel } from "@mui/material";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+
 interface TodoProps {
-  title: string;
-  due: Date;
-  isCompleted: boolean;
-  toggleCompleted: () => void;
+  todoDetails: {
+    id: number;
+    title: string;
+    plannedStart?: Date;
+    plannedEnd?: Date;
+    isCompleted: boolean;
+  };
+  toggleCompleted: (id: number) => void;
 }
 
-export const Todo: React.FC<TodoProps> = ({ title, due, isCompleted, toggleCompleted }) => {
-  return <div className="Todo">Hello universe</div>;
+export const Todo: React.FC<TodoProps> = ({ todoDetails, toggleCompleted }) => {
+  const { id, title, plannedStart, plannedEnd, isCompleted } = todoDetails;
+
+  return (
+    <div className="Todo">
+      <FormControlLabel
+        label={title}
+        control={
+          <Checkbox
+            className="todo-checkbox"
+            checked={isCompleted}
+            inputProps={{ "aria-label": "controlled" }}
+            onChange={() => toggleCompleted(id)}
+          />
+        }
+      />
+      <div className="planned-container">
+        <CheckCircleOutlineIcon />
+        <span>
+          {plannedStart?.toLocaleDateString()} - {plannedEnd?.toLocaleDateString()}
+        </span>
+      </div>
+    </div>
+  );
 };
