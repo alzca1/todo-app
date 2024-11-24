53e0c791fcb8d6252818cb34fc723203331779b6
Álvaro Zájara <alvaro.zajara@eviden.com>
2024-11-24 11:07:07 +0100

    added necessary config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: ``, // Importa variables globales automáticamente
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith(".css")) {
+            return "assets/css/[name]-[hash][extname]"; // Genera un archivo CSS con hash único
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
+});
